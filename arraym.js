bd9ff4b10535c83f7f67f3b2dc7b07780e7b4cda
Birendra <v-bipandit@microsoft.com>
2025-08-03 20:44:40 +0530

    sdjh

--- arraym.js
+++ arraym.js
@@ -1,17 +1,42 @@
-var fruit = ['apple', 'banana', 'cherry'];
-var udatefruit = fruit.push("kiwy");
-console.log(udatefruit); // Output: 4 updatefruit give length of udated fruit array
-console.log(fruit); // Output: ['apple', 'banana', 'cherry', 'kiwy']
-// const removedFruit = fruit.pop();
-// console.log(removedFruit); // Output: 'kiwy' removedFruit give the last element of fruit array
-var index = fruit.indexOf('banana');
-fruit.splice(index, 1);
-console.log(fruit); // Output: ['apple', 'cherry']
-var newFruit = ['orange', 'grape'];
-newFruit.reverse();
-console.log(newFruit); // Output: ['grape', 'orange']
-var combinedFruits = fruit.concat(newFruit);
-console.log(combinedFruits); //
-// Output: ['apple', 'cherry', 'grape', 'orange'] 
-combinedFruits.splice(1, 2, 'grape', 'mango');
-console.log(combinedFruits); // Output: ['apple', 'orange']
+var array = [1, 2, 3, 3, 4,];
+var skdd = 2228;
+function hello() {
+    console.log("Hello, World!");
+}
+function addNumbers(a, b) {
+    return a + b;
+}
+function findMax(arr) {
+    return Math.max.apply(Math, arr);
+}
+function findMin(arr) {
+    return Math.min.apply(Math, arr);
+}
+function removeDuplicates(arr) {
+    return Array.from(new Set(arr));
+}
+function sortArray(arr) {
+    return arr.slice().sort(function (a, b) { return a - b; });
+}
+function sumArray(arr) {
+    return arr.reduce(function (acc, curr) { return acc + curr; }, 0);
+}
+function averageArray(arr) {
+    return sumArray(arr) / arr.length;
+}
+function reverseArray(arr) {
+    return arr.slice().reverse();
+}
+function isArrayEmpty(arr) {
+    return arr.length === 0;
+}
+function arrayContains(arr, value) {
+    return arr.includes(value);
+}
+function filterArray(arr, predicate) {
+    return arr.filter(predicate);
+}
+function mapArray(arr, callback) {
+    return arr.map(callback);
+}
+console.log("function string" + " " + hello());
--- arraym.ts
+++ arraym.ts
@@ -2,3 +2,61 @@
 const array =[1,2,3,3,4,];
 
 const skdd = 2228;
+
+function hello() {
+    console.log("Hello, World!");
+}
+
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+function findMax(arr: number[]): number {
+    return Math.max(...arr);
+}
+
+function findMin(arr: number[]): number {
+    return Math.min(...arr);
+}
+
+function removeDuplicates(arr: number[]): number[] {
+    return Array.from(new Set(arr));
+}
+
+function sortArray(arr: number[]): number[] {
+    return arr.slice().sort((a, b) => a - b);
+}
+
+function sumArray(arr: number[]): number {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+
+
+function averageArray(arr: number[]): number {
+    return sumArray(arr) / arr.length;
+}
+
+function reverseArray(arr: number[]): number[] {
+    return arr.slice().reverse();
+}
+
+function isArrayEmpty(arr: any[]): boolean {
+    return arr.length === 0;
+}
+
+function arrayContains(arr: any[], value: any): boolean {
+    return arr.includes(value);
+}
+
+function filterArray(arr: any[], predicate: (value: any) => boolean): any[] {
+    return arr.filter(predicate);
+}
+
+
+function mapArray<T, U>(arr: T[], callback: (value: T) => U): U[] {
+    return arr.map(callback);
+}
+
+
+console.log("function string" + " " + hello());
